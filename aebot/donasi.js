6629574b730090494d1242eb6b95851b67e8321e
KanzGG <78731231+KanzGG@users.noreply.github.com>
2021-02-09 23:57:27 +0800

    Add files via upload

---
+++ aebot/donasi.js
@@ -0,0 +1,21 @@
+const donasi = (Ig, name) => { 
+	return 
+	 `     
+             𝗗𝗢𝗡𝗔𝗦𝗜  
+
+     *DONASI SEIKHLASNYA:)* 
+ 
+  *BOT BY ${KanzBOT}*
+
+
+
+
+Note:
+Buat Yg Gk Donasi Gppkok)
+
+${082148505738} 
+
+     `
+}
+
+exports.donasi = donasi
